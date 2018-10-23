298491b09421d75cb33bc2151f235b8d53b03549
isuperflying <1365961935@qq.com>
2018-10-23 17:51:38 +0800

    fix bugs

--- pages/home1/home1.js
+++ pages/home1/home1.js
@@ -15,6 +15,25 @@ Page({
     wx.setNavigationBarTitle({
       title: '萌宝学单词'
     })
+
+    var animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: "ease",
+      delay: 0
+    })
+    let bottom = 350
+    let x = 1
+    setInterval(function () {
+      bottom = 350 + Math.cos(x++) * 10
+      if (x > 1000) {
+        x = 1
+      }
+      animation.bottom(bottom + "rpx").step()
+      this.setData({
+        animationData: animation.export()
+      })
+    }.bind(this), 300)
+
   },
 
   basetrain: function () {
--- pages/testpay/testpay.js
+++ pages/testpay/testpay.js
@@ -1,4 +1,7 @@
 // pages/testpay/testpay.js
+let wechat = require('../../utils/wechat.js');
+var app = getApp();
+let userInfo
 Page({
 
   /**
@@ -16,74 +19,70 @@ Page({
   },
 
   paytest:function(){
-    var that = this
-    wx.login({
-      success: function (res) {
-        if (res.code) {
-          //发起网络请求
-          wx.request({
-            url: 'http://192.168.80.97:8888/userLogin',
-            method: 'POST',
-            data: {
-              code: res.code
-            },
-            success: function (res) {
-              console.log(res.data.data.openid)
-              let openid = res.data.data.openid
-              that.getPayInfo(openid)
-            },
-            fail: function (err) {
-              console.log(err)
-            }
-          })
-        } else {
-          console.log('获取用户登录态失败！' + res.errMsg)
-        }
-      }
-    });
+    userInfo = app.globalData.userInfo || wx.getStorageSync('user_info')
+    this.getPayInfo()
   },
 
-  getPayInfo:function(openid){
+  getPayInfo:function(){
     wx.request({
       url: 'http://192.168.80.97:8888/getpayinfo',
       method: 'POST',
       data: {
-        openid: openid
+        openid: userInfo.openId,
+        token: userInfo.token
       },
       success: function (res) {
-        console.log(res.data.data)
+        console.log(res.data)
+        if(res.data.code == 0){
+          var timestamp = res.data.data.timestamp + '' //时间戳
+          var nonceStr = res.data.data.nonceStr  //随机数
+          var packages = res.data.data.package  //prepay_id
+          var paySign = res.data.data.paySign  //签名
+          var signType = 'MD5'
 
-        var timestamp = res.data.data.timestamp +'' //时间戳
-        var nonceStr = res.data.data.nonceStr  //随机数
-        var packages = res.data.data.package  //prepay_id
-        var paySign = res.data.data.paySign  //签名
-        var signType = 'MD5'
-
-        wx.requestPayment({
-          timeStamp: timestamp,
-          nonceStr: nonceStr,
-          package: packages,
-          signType: signType,
-          paySign: paySign,
-          success: function (res) {
-            console.log(res)
-          },
-          fail:function(res){
-            console.log(res)
-          }
-        })
-        
+          wx.requestPayment({
+            timeStamp: timestamp,
+            nonceStr: nonceStr,
+            package: packages,
+            signType: signType,
+            paySign: paySign,
+            success: function (res) {
+              console.log(res)
+            },
+            fail: function (res) {
+              console.log(res)
+            }
+          })
+        }else{
+          wx.showToast({
+            title: '数据异常，请重试',
+            icon:'none'
+          })
+        }
       },
       fail: function (err) {
         console.log(err)
       }
     })
   },
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+  
+  userLogin:function(e){
+    this.getUserInfo();
+  },
 
+  getUserInfo() {
+    wechat.getCryptoData2()
+      .then(d => {
+        return wechat.getMyData(d);
+      })
+      .then(d => {
+        console.log("从后端获取的openid", d.data);
+        wechat.saveUserInfo(d.data.data)
+        app.globalData.userInfo = d.data.data
+      })
+      .catch(e => {
+        console.log(e);
+      })
   },
 
   /**
